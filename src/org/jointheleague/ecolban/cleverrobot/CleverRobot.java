47cb31e8e470689976fa6bdc86b4acdc9b05c68f
Gabriel C <league@WTS-iMac-11.attlocal.net>
2017-06-18 11:04:22 -0700

    Ported to eclipse from old GreenBot2016

--- src/org/jointheleague/ecolban/cleverrobot/CleverRobot.java
+++ src/org/jointheleague/ecolban/cleverrobot/CleverRobot.java
@@ -5,10 +5,13 @@ package org.jointheleague.ecolban.cleverrobot;
  * version 0.9, 170227
  **********************************************************************************************/
 import java.io.IOException;
+import java.util.Random;
 
 import org.jointheleague.ecolban.rpirobot.IRobotAdapter;
 import org.jointheleague.ecolban.rpirobot.IRobotInterface;
 import org.jointheleague.ecolban.rpirobot.SimpleIRobot;
+
+
 
 public class CleverRobot extends IRobotAdapter {
 	Sonar sonar = new Sonar();
@@ -36,19 +39,103 @@ public class CleverRobot extends IRobotAdapter {
 		//	System.out.println(i);
 		//}
 	}
+	public boolean loop() throws InterruptedException, IOException  {
+        readSensors(SENSORS_GROUP_ID100);
 
-	private boolean loop() throws Exception {
-		//System.out.println("LEFT SONAR: " + sonar.readSonar("left"));
-		Thread.sleep(1000);
-		//setTailLight(tailLight = !tailLight);
-		//System.out.println("RIGHT SONAR: " + sonar.readSonar("right"));
-		//System.out.println("CENTER SONAR: " + sonar.readSonar("center"));
-		cam.takeRGBPicture();
-		System.out.println(cam.getRedPercentage(15,true));
-		System.out.println(cam.getBluePercentage(15,false));
-		System.out.println(cam.getGreenPercentage(15,false));
-		return true;
-	}
+        leds(139, 4, true);
+        rightmaze();
+        return true;
+
+    }
+
+    void rightmaze() throws InterruptedException, IOException  {
+        System.out.println(getWallSignal() + "");
+        int Mlights[] = getLightBumps();
+        drive(500, -220);
+        if (Mlights[5] > 1 || Mlights[4] > 1 || Mlights[3] > 1) {
+            driveDirect(-220, 500);
+            Thread.sleep(3);
+
+
+        }
+    }
+
+    public void leftmaze() throws InterruptedException, IOException {
+        System.out.println(getWallSignal() + "");
+        int Mlights[] = getLightBumps();
+        drive(500, 220);
+        if (Mlights[0] > 1 || Mlights[1] > 1 || Mlights[2] > 1) {
+            driveDirect(500, -220);
+            Thread.sleep(3);
+
+        }
+    }
+
+    public void Goldrush() throws InterruptedException, IOException  {
+        int Mlights[] = getLightBumps();
+System.out.println("Running Gold");
+        if (getInfraredByte() == 248 || getInfraredByte() == 250) {
+//right infared
+            driveDirect(-100, 100);
+        } else if (getInfraredByte() == 248 || getInfraredByte() == 250) {
+            driveDirect(100, -100);
+        } else {
+            int r = new Random().nextInt(2);
+
+            if (r == 1) {
+                drive(500, 220);
+                if (Mlights[0] > 1 || Mlights[1] > 1 || Mlights[2] > 1) {
+                    driveDirect(500, -220);
+                    Thread.sleep(3);
+                }
+            } else {
+                drive(500, -220);
+                if (Mlights[5] > 1 || Mlights[4] > 1 || Mlights[3] > 1) {
+                    driveDirect(-220, 500);
+                    Thread.sleep(3);
+                }
+            }
+            Thread.sleep(100);
+        }
+    }
+
+
+    public void Dragrace() throws IOException, InterruptedException  {
+        driveDirect(500, 500);
+        int[] lights = getLightBumps();
+        //Turn Left
+        if (isBumpLeft() && isBumpRight()) {
+            System.out.println("Turn around");
+            driveDirect(-400, -400);
+            Thread.sleep(1400);
+            driveDirect(500, -500);
+            Thread.sleep(900);
+        }
+
+
+        //Turn Around
+
+        else if (lights[5] > 1 || lights[4] > 1 || lights[3] > 1) {
+            driveDirect(0, 500);
+
+            System.out.println("Right");
+            Thread.sleep(50);
+        } else if (lights[0] > 1 || lights[1] > 1 || lights[2] > 1) {
+            driveDirect(500, 0);
+            System.out.println("left");
+            Thread.sleep(50);
+        } else if (isBumpRight()) {
+            driveDirect(-500, -500);
+            Thread.sleep(300);
+            driveDirect(100, 500);
+            Thread.sleep(100);
+        } else if (isBumpLeft()) {
+            driveDirect(-500, -500);
+            Thread.sleep(300);
+            driveDirect(500, 100);
+            Thread.sleep(100);
+        }
+        }
 
 	private void shutDown() throws IOException {
 		reset();
