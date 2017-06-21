a026d1690e7b46de1b80c15defea91477d9163d4
pandg3 <pauloacampbell@icloud.com>
2017-06-21 16:28:10 -0700

    DrivePWM

--- src/org/jointheleague/ecolban/cleverrobot/CleverRobot.java
+++ src/org/jointheleague/ecolban/cleverrobot/CleverRobot.java
@@ -12,12 +12,11 @@ import org.jointheleague.ecolban.rpirobot.IRobotInterface;
 import org.jointheleague.ecolban.rpirobot.SerialConnection;
 import org.jointheleague.ecolban.rpirobot.SimpleIRobot;
 
-
-
 public class CleverRobot extends IRobotAdapter {
 	Sonar sonar = new Sonar();
 	private boolean tailLight;
 	Camera cam;
+
 	public CleverRobot(IRobotInterface iRobot) {
 		super(iRobot);
 	}
@@ -30,118 +29,120 @@ public class CleverRobot extends IRobotAdapter {
 		while (rob.loop()) {
 		}
 		rob.shutDown();
-		
 
 	}
-	
 
-	public boolean loop() throws Exception  {
-        readSensors(SENSORS_GROUP_ID100);
+	public boolean loop() throws Exception {
+		readSensors(SENSORS_GROUP_ID100);
+		Dragrace();
+		//leds(139, 4, true);
+		// Select Code
+//		rightmaze();
+		return true;
 
-        leds(139, 4, true);
-        //Select Code
-        driveDirect(400, 400);
-        
-        return true;
+	}
 
-    }
+	void rightmaze() throws InterruptedException, IOException {
+		System.out.println(getWallSignal() + "");
+		int Mlights[] = getLightBumps();
+		drive(500, -220);
+		if (Mlights[5] > 1 || Mlights[4] > 1 || Mlights[3] > 1) {
+			driveDirect(-220, 500);
+			Thread.sleep(3);
 
-    void rightmaze() throws InterruptedException, IOException  {
-        System.out.println(getWallSignal() + "");
-        int Mlights[] = getLightBumps();
-        drive(500, -220);
-        if (Mlights[5] > 1 || Mlights[4] > 1 || Mlights[3] > 1) {
-            driveDirect(-220, 500);
-            Thread.sleep(3);
+		}
+	}
 
-
-        }
-    }
-
-    private void setup() throws Exception {
-		//driveDirect(100, 100);
-		cam = new Camera(100,100);
-		//for(int i : cam.pixels){
-		//	System.out.println(i);
-		//}
+	private void setup() throws Exception {
+		
+		//drivePWM(255, 255); //13.37 seconds
+		driveDirect(500, 500);
+		//cam = new Camera(100, 100);
+		// for(int i : cam.pixels){
+		// System.out.println(i);
+		// }
 	}
 
 	public void leftmaze() throws InterruptedException, IOException {
-        System.out.println(getWallSignal() + "");
-        int Mlights[] = getLightBumps();
-        drive(500, 220);
-        if (Mlights[0] > 1 || Mlights[1] > 1 || Mlights[2] > 1) {
-            driveDirect(500, -220);
-            Thread.sleep(3);
+		System.out.println(getWallSignal() + "");
+		int Mlights[] = getLightBumps();
+		drive(500, 220);
+		if (Mlights[0] > 1 || Mlights[1] > 1 || Mlights[2] > 1) {
+			driveDirect(500, -220);
+			Thread.sleep(3);
 
-        }
-    }
+		}
+	}
 
-    public void Goldrush() throws InterruptedException, IOException  {
-        int Mlights[] = getLightBumps();
-System.out.println("Running Gold");
-        if (getInfraredByte() == 248 || getInfraredByte() == 250) {
-//right infared
-            driveDirect(-100, 100);
-        } else if (getInfraredByte() == 248 || getInfraredByte() == 250) {
-            driveDirect(100, -100);
-        } else {
-            int r = new Random().nextInt(2);
+	public void Goldrush() throws InterruptedException, IOException {
+		int Mlights[] = getLightBumps();
+		System.out.println("Running Gold");
+		if (getInfraredByte() == 248 || getInfraredByte() == 250) {
+			// right infared
+			driveDirect(-100, 100);
+		} else if (getInfraredByte() == 248 || getInfraredByte() == 250) {
+			driveDirect(100, -100);
+		} else {
+			int r = new Random().nextInt(2);
 
-            if (r == 1) {
-                drive(500, 220);
-                if (Mlights[0] > 1 || Mlights[1] > 1 || Mlights[2] > 1) {
-                    driveDirect(500, -220);
-                    Thread.sleep(3);
-                }
-            } else {
-                drive(500, -220);
-                if (Mlights[5] > 1 || Mlights[4] > 1 || Mlights[3] > 1) {
-                    driveDirect(-220, 500);
-                    Thread.sleep(3);
-                }
-            }
-            Thread.sleep(100);
-        }
-    }
+			if (r == 1) {
+				drive(500, 220);
+				if (Mlights[0] > 1 || Mlights[1] > 1 || Mlights[2] > 1) {
+					driveDirect(500, -220);
+					Thread.sleep(3);
+				}
+			} else {
+				drive(500, -220);
+				if (Mlights[5] > 1 || Mlights[4] > 1 || Mlights[3] > 1) {
+					driveDirect(-220, 500);
+					Thread.sleep(3);
+				}
+			}
+			Thread.sleep(100);
+		}
+	}
 
+	public void Dragrace() throws IOException, InterruptedException {
+		
+		int[] lights = getLightBumps();
+		// Turn Left
+		if (isBumpLeft() && isBumpRight()) {
+			System.out.println("Turn around");
+			driveDirect(-400, -400);
+			Thread.sleep(1400);
+			driveDirect(500, -500);
+			Thread.sleep(900);
+			drivePWM(255, 255);
+		}
 
-    public void Dragrace() throws Exception  {
-        drivePWM(256,256);
-        int[] lights = getLightBumps();
-        //Turn Left
-        if (isBumpLeft() && isBumpRight()) {
-            System.out.println("Turn around");
-            driveDirect(-400, -400);
-            Thread.sleep(1400);
-            driveDirect(500, -500);
-            Thread.sleep(900);
-        }
+		// Turn Around
 
+		else if (lights[5] > 1 || lights[4] > 1 || lights[3] > 1) {
+			driveDirect(0, 500);
 
-        //Turn Around
-
-        else if (lights[5] > 1 || lights[4] > 1 || lights[3] > 1) {
-            driveDirect(0, 500);
-
-            System.out.println("Right");
-            Thread.sleep(50);
-        } else if (lights[0] > 1 || lights[1] > 1 || lights[2] > 1) {
-            driveDirect(500, 0);
-            System.out.println("left");
-            Thread.sleep(50);
-        } else if (isBumpRight()) {
-            driveDirect(-500, -500);
-            Thread.sleep(300);
-            driveDirect(100, 500);
-            Thread.sleep(100);
-        } else if (isBumpLeft()) {
-            driveDirect(-500, -500);
-            Thread.sleep(300);
-            driveDirect(500, 100);
-            Thread.sleep(100);
-        }
-        }
+			System.out.println("Right");
+			Thread.sleep(500);
+			drivePWM(255, 255);
+		} else if (lights[0] > 1 || lights[1] > 1 || lights[2] > 1) {
+			driveDirect(500, 0);
+			System.out.println("left");
+			Thread.sleep(500);
+			drivePWM(255, 255);
+		} 
+		else if (isBumpRight()) {
+			driveDirect(-500, -500);
+			Thread.sleep(300);
+			driveDirect(100, 500);
+			Thread.sleep(100);
+			drivePWM(255, 255);
+		} else if (isBumpLeft()) {
+			driveDirect(-500, -500);
+			Thread.sleep(300);
+			driveDirect(500, 100);
+			Thread.sleep(100);
+			drivePWM(255, 255);
+		}
+	}
 
 	private void shutDown() throws IOException {
 		reset();
